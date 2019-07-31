f3b57d194ed8a4b21d2664fa92ba8fe00775a04e
katrennada <bouchaira.katrennada@gmail.com>
2019-07-31 16:54:36 +0200

    Floating-intercept and Close-in path loss models

--- path_loss_model.py
+++ path_loss_model.py
@@ -25,7 +25,7 @@ def get_path_loss_fi_model(alpha,beta,sigma):
     x = np.random.randn(10000) * sigma + mu
     ksi = np.mean(x)
     #Omnidirectiona path loss in dB
-    path_loss= alpha+10*beta*np.log10(d)+float(ksi)
+    path_loss= beta+10*alpha*np.log10(d)+float(ksi)
     return(path_loss)
 
 def get_free_space_path_loss(d,f):
@@ -42,25 +42,44 @@ def get_free_space_path_loss(d,f):
     fspl= 20*np.log10(d)+20*math.log10(f)+32.45
     return(fspl)
 
+def get_path_loss_ci_model(d,f,n,sigma):
+    """
+    Parameters
+    ----------
+    d : distance in meters
+    f : frequence in GHz
+    n :
+    sigma :
+    Returns
+    -------
+    path_loss : Omnidirectional Path loss in dB
+    """
+    mu = 0
+    x = np.random.randn(10000) * sigma + mu
+    ksi = np.mean(x)
+    #free space path loss in dB
+    d0=1
+    fspl= 20*np.log10(d0)+20*math.log10(f)+32.45
+    path_loss= fspl+10*n*np.log10(d/d0)+float(ksi)
+    return(path_loss)
 
 if __name__ == "__main__":
     f = float(input("Enter frequency in GHz "))
-    alpha = float(input("Enter alpha "))
-    beta = float(input("Enter beta "))
-    sigma = float(input("Enter sigma "))
+    alpha_fi = float(input("Enter alpha_fi "))
+    beta_fi = float(input("Enter beta_fi "))
+    sigma_fi = float(input("Enter sigma_fi "))
 
-    alpha1 = float(input("Enter alpha1 "))
-    beta1 = float(input("Enter beta1 "))
-    sigma1 = float(input("Enter sigma1 "))
+    n_ci = float(input("Enter n_ci "))
+    sigma_ci = float(input("Enter sigma_ci "))
 
     d = np.linspace(30,200,100)
-    pl= get_path_loss_fi_model(alpha,beta,sigma)
-    pl1= get_path_loss_fi_model(alpha1,beta1,sigma1)
+    pl_fi= get_path_loss_fi_model(alpha_fi,beta_fi,sigma_fi)
+    pl_ci= get_path_loss_ci_model(d,f,n_ci,sigma_ci)
     fspl= get_free_space_path_loss(d,f)
 
     plt.semilogx(d,fspl,"^",label="Free space path loss")
-    plt.plot(d,pl,label="LOS")
-    plt.plot(d,pl1, label="NLOS")
+    plt.plot(d,pl_fi,label="FI model")
+    plt.plot(d,pl_ci, label="CI model")
     plt.title("Path loss = f(d)")
     plt.legend()
     plt.xlabel("Distance (m)")
